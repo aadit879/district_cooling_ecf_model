59c1d2c135239b74a36e1a5c6643a6ed3c171cdb
aadit <malla@eeg.tuwien.ac.at>
2024-02-19 11:05:58 +0100

    add test input rasters

---
+++ cm/app/api_v1/test_scripts_call.py
@@ -0,0 +1,35 @@
+import os
+os.chdir(os.path.join(os.getcwd(), r'cm\app\api_v1\my_calculation_module_directory'))
+
+import sys
+sys.path.append('.')
+
+current_directory = os.getcwd()
+
+# Get the path to the 'api_v1' directory by going one level up
+api_v1_directory = os.path.dirname(current_directory)
+
+# Add the 'api_v1' directory to sys.path
+sys.path.append(api_v1_directory)
+
+## change the helper and constant import statements in constant.py
+
+import calculation_module
+from cm.app import helper
+from flask import request
+
+data = request.get_json()
+
+inputs_raster_selection = helper.validateJSON(data["inputs_raster_selection"])
+
+
+inputs_parameter_selection = helper.validateJSON(data["inputs_parameter_selection"])
+
+
+
+inputs_vector_selection = helper.validateJSON(data["inputs_vector_selection"])
+
+root_directory = os.getcwd()
+output_directory =  root_directory + '\\Output\\'
+
+result = calculation_module.calculation(output_directory, inputs_raster_selection,inputs_vector_selection,inputs_parameter_selection)
--- cm/app/constant.py
+++ cm/app/constant.py
@@ -21,30 +21,103 @@ PORT = PORT_DOCKER
 
 TRANFER_PROTOCOLE ='http://'
 INPUTS_CALCULATION_MODULE = [
-    {'input_name': 'Multiplication factor',
+    {'input_name': 'Average Electricity Price',
      'input_type': 'input',
-     'input_parameter_name': 'multiplication_factor',
-     'input_value': '1',
+     'input_parameter_name': 'electricity_prices',
+     'input_value': '30',
      'input_priority': 0,
-     'input_unit': '',
+     'input_unit': 'Euro/MWh',
      'input_min': 0,
-     'input_max': 10, 'cm_id': CM_ID  # Do no change this value
+     'input_max': 1000, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'Estimated Cooling Days in a year',
+     'input_type': 'input',
+     'input_parameter_name': 'flh_cooling_days',
+     'input_value': '60',
+     'input_priority': 0,
+     'input_unit': 'Days',
+     'input_min': 0,
+     'input_max': 300, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'COP District Cooling Supply Technology',
+     'input_type': 'input',
+     'input_parameter_name': 'COP_DC',
+     'input_value': '4.89',
+     'input_priority': 0,
+     'input_unit': '%',
+     'input_min': 0,
+     'input_max': 100, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'Network Operating Conditions',
+     'input_type': 'input',
+     'input_parameter_name': 'delta_T_dc',
+     'input_value': '10',
+     'input_priority': 0,
+     'input_unit': '%',
+     'input_min': 10,
+     'input_max': 100, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'COP Individual Cooling Supply Technology',
+     'input_type': 'input',
+     'input_parameter_name': 'idnd_tec_SEER',
+     'input_value': '3.6',
+     'input_priority': 0,
+     'input_unit': '%',
+     'input_min': 0,
+     'input_max': 100, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'Interest Rate',
+     'input_type': 'input',
+     'input_parameter_name': 'interest_rate',
+     'input_value': '6',
+     'input_priority': 1,
+     'input_unit': '%',
+     'input_min': 0,
+     'input_max': 1, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'Depreciation Time District Cooling',
+     'input_type': 'input',
+     'input_parameter_name': 'decpreciation_dc',
+     'input_value': '25',
+     'input_priority': 1,
+     'input_unit': 'years',
+     'input_min': 0,
+     'input_max': 100, 'cm_id': CM_ID  # Do no change this value
+     },
+
+    {'input_name': 'Depreciation Time Individual Cooling',
+     'input_type': 'input',
+     'input_parameter_name': 'decpreciation_ind',
+     'input_value': '15',
+     'input_priority': 1,
+     'input_unit': 'years',
+     'input_min': 0,
+     'input_max': 100, 'cm_id': CM_ID  # Do no change this value
      },
 ]
 
 
 SIGNATURE = {
 
-    "category": "Demand",
+     "category": "Demand",
+    "authorized_scale":["LAU 2","Hectare"],
     "cm_name": CM_NAME,
-    "wiki_url": "https://wiki.hotmaps.hevs.ch/en/CM-Scale-heat-and-cool-density-maps",
-    "layers_needed": [],
+    "wiki_url": "https://wiki.hotmaps.hevs.ch/en/CM-Scale-heat-and-cool-density-maps", #TODO AM: Needs to be updated
+    "layers_needed": ["heat_tot_curr_density_tif","gfa_tot_curr_density_tif","gfa_nonres_curr_density_tif"], # AM: nonres to be selected by default (for now)
     "type_layer_needed": [
-        {"type": "heat", "description": "Choose a heat demand density layer."}
+        {"type": "heat", "description": "Select the cold demand density layer"},#TODO AM: What is the 'type' for cooling?
+        {"type": "gross_floor_area", "description": "Select the gross floor area density layer"}
+        #{"type": "gross_floor_area", "description": "Select the gross floor area density layer"}
     ],
-    "type_vectors_needed": [],
+    "type_vectors_needed": [], ### AM: to be added; most likely convert the vector files to raster
     "cm_url": "Do not add something",
-    "cm_description": "This calculation module allows to scale the heat demand density layer up or down.",
+    "cm_description": "This calculation module allows the identification of areas with district cooling potential",
     "cm_id": CM_ID,
     'inputs_calculation_module': INPUTS_CALCULATION_MODULE
 }
